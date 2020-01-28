de5a19cec268fa81b11276f82fec91334e41aca8
vienpham2019 <vienpham2019@gmail.com>
2020-01-28 17:31:01 -0600

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,31 @@
+class Book 
 
+    attr_accessor :title , :author , :page_count , :genre 
+    def initialize(title)
+        @title = title
+    end
+    def title
+        @title
+    end
+    def author=(author)
+        @author =  author
+    end
+    def page_count=(page_num)
+        @page_count = page_num
+    end
+    def page_count
+        @page_count
+    end
+    def genre=(genre)
+        @genre = genre
+    end
+    def genre
+        @genre
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+    
+
+end
 
